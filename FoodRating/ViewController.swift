943fbe9e8178168d28d96c7d5277309a4a6e8be7
Callum Jones <13146598@asn-stu-m-1074.ad.mmu.ac.uk>
2018-02-19 14:45:36 +0000

    Initial Commit

---
+++ FoodRating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodRating
+//
+//  Created by Callum Jones on 19/02/2018.
+//  Copyright © 2018 Callum Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
